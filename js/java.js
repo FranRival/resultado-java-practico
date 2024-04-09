3ef4be7b6a0f3a3d4d32d7ea45686174617fc53d
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-08 22:19:10 -0600

    se crearon 2 constantes, una de menuCarritoIcon, y otra de aside. ahi estaba el big detail. and i dont know porque eso happend.

--- js/java.js
+++ js/java.js
@@ -1,13 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
-const menuCarritoIcon = document.querySelector('.product-detail')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const aside = document.querySelector('.product-detail')
+
+//el error: al mee intentarlo, no tome en cuenta que tambien la imagen debe ser clikeable. 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
-menuCarritoIcon.addEventListener('click', funs)
+menuCarritoIcon.addEventListener('click', toggleCarritoMenu)
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive')
@@ -18,6 +21,6 @@ function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
 }
 
-function funs (){
-    menuCarritoIcon.classList.toggle('inactive')
+function toggleCarritoMenu (){
+    aside.classList.toggle('inactive')
 }
