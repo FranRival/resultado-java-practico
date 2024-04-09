89af7cca13ea2238b475c5348c8a9f529a2f4e5a
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-08 22:10:29 -0600

    intente hacer el mismo proceso anterior a la inversa. con el carrito de compras para desplegarlo, butt, no pude.

--- js/java.js
+++ js/java.js
@@ -1,19 +1,23 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.product-detail')
 const mobileMenu = document.querySelector('.mobile-menu')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', funs)
 
 function toggleDesktopMenu (){
-    //se selecciono la clase inactve.
-    //ejecuta, quita o pone la clase inactive, si la tiene o no 
     desktopMenu.classList.toggle('inactive')
 
 }
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
+}
+
+function funs (){
+    menuCarritoIcon.classList.toggle('inactive')
 }
