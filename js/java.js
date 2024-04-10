2118c56d7250cf36cc3287641c7d9a84049d47f0
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-09 23:16:09 -0600

    el ciclo for que estamos haciendo en la linea 59... es magia...

--- js/java.js
+++ js/java.js
@@ -56,3 +56,6 @@ productList.push({
     imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
+for (product of productList) { // magia?
+    document.createElement()
+}
