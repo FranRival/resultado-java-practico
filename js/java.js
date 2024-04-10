be5afd3b97c0c34d64b6d45c939452ec40535d75
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-09 23:23:55 -0600

    estructurando la data con js....

--- js/java.js
+++ js/java.js
@@ -56,6 +56,38 @@ productList.push({
     imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 })
 
+
+
+/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> */
+
+
 for (product of productList) { // magia?
-    document.createElement()
+    const productCard = document.createElement('div')
+    productCard.classList.add('product-card') //le agregamos una clase
+
+    //product = {name, price, img} -> produt.image
+    const img = document.createElement('img')
+    img.setAttribute('src', product.imagen)
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-card') 
+
+    const productInfoDiv = document.createElement('div')
+
+    const productPrice = document.createElement('div')
+    productPrice.innerText = '$' + product.price
+
+    const productName = document.createElement('div')
+    productName.innerText = product.name
 }
